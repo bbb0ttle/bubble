8c882a3997851a5358ea9da92c357de678362ac4
bbbottle <b@bbki.ng>
2025-08-21 04:08:59 +0000

    feat: add size attr

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,45 @@ class BBBubble extends HTMLElement {
         `;
     }
 
+    size: number = 128;
+    minSize: number = 30;
+    maxSize: number = 300;
+
+    getSafeSize(size: number): number {
+        if (size < this.minSize) {
+            return this.minSize;
+        } else if (size > this.maxSize) {
+            return this.maxSize;
+        }
+        return size;
+    }
+
+    updateSize(newSize: number) {
+        this.size = this.getSafeSize(newSize);
+        (this.root.querySelector('.bubble') as HTMLElement)!.style.width = `${this.size}px`;
+        (this.root.querySelector('.bubble') as HTMLElement)!.style.height = `${this.size}px`;
+    }
+
+    static get observedAttributes() {
+        return ['size'];
+    }
+
+    attributeChangedCallback(name: string, _oldValue: string, newValue: string) {
+        if (name === 'size') {
+            this.updateSize(parseInt(newValue, 10));
+        }
+    }
+
     connectedCallback() {
         const stylesheet = new CSSStyleSheet();
         stylesheet.replaceSync(css());
+
+        // init size from attr
+        const sizeAttr = this.getAttribute('size');
+        if (sizeAttr) {
+            this.updateSize(parseInt(sizeAttr, 10));
+        }
+
         this.root.adoptedStyleSheets = [stylesheet];
     }
 }
