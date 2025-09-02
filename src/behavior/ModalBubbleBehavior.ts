31fd4dc5dfafa2a4ea6c253444edd1e4acfc6090
bbbottle <b@bbki.ng>
2025-09-02 22:29:22 +0800

    chore: bump

--- src/behavior/ModalBubbleBehavior.ts
+++ src/behavior/ModalBubbleBehavior.ts
@@ -62,7 +62,7 @@ export class ModalBubbleBehavior implements BubbleBehavior{
         
     };
 
-    private _ver: string = "0.2.3-a.39";
+    private _ver: string = "0.2.3-a.41";
 
     private _fullscreen = false;
 
